840a53a40a9577b7428b83ed350c42972a16c488
MTofayelAhmed <tofayelahmed324@gmail.com>
2026-02-19 23:19:22 +0100

    done with post

--- src/server.ts
+++ src/server.ts
@@ -51,9 +51,21 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello Next level Developer!')
 })
 
-app.post("/", (req: Request, res: Response) => {
-    console.log(req.body)
-    res.status(201).json({ success: true, message: "Data received successfully" });
+app.post("/users", async (req: Request, res: Response) => {
+    
+const {name, email}= req.body
+
+try {
+  const result = await pool.query(`INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *`, [name, email])
+    console.log(result.rows[0])
+
+    res.status(201).json({ success: true, message: "User inserted successfully", data: result.rows[0] })
+
+} catch (error : any) {
+    res.status(500).json({ success: false, message: error.message });
+}
+
+   
 });
 
 app.listen(port, () => {
