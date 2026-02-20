ab4b7e16dc44990a7e91124d7739d9797bf24fe3
MTofayelAhmed <tofayelahmed324@gmail.com>
2026-02-20 01:17:05 +0100

    done with todos post and get api

--- src/server.ts
+++ src/server.ts
@@ -122,6 +122,28 @@ app.delete("/users/:id", async (req: Request, res: Response)=>{
 
 })
 
+
+// todos crud operations
+app.post("/todos", async (req: Request, res: Response) => {
+    
+    const {user_id, title}= req.body
+    try {
+      const result = await pool.query(`INSERT INTO todos (user_id, title) VALUES($1, $2) RETURNING *`, [user_id, title])
+      res.status(201).json({ success: true, message: "Todo inserted successfully", data: result.rows[0] })
+    } catch (error : any) {
+      res.status(500).json({ success: false, message: error.message });
+    }
+})
+
+app.get("/todos", async(req: Request, res: Response)=>{
+    try {
+        const result = await pool.query(`SELECT * FROM todos`)
+        res.status(200).json({ success: true, data: result.rows })
+    } catch (error : any) {
+        res.status(500).json({ success: false, message: error.message, details: error });
+    }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
